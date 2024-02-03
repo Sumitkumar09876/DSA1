de8cf0dcc97e2b9ba9a9e00704087d649cf4d705
Sumitkumar09876 <skumar21-comp@bvucoep.edu.in>
2024-02-03 12:22:48 +0530

    update

--- STL/8Map.cpp
+++ STL/8Map.cpp
@@ -7,8 +7,14 @@ int main(){
     m[2]="kesav";
     m[3]="Rahul";
     for(auto i:m){
-        cout<<i.first;
+        cout<<i.first<<" ";
     }
+    cout<<"Count element:"<<m.count(3);
+    m.erase(3);
+    auto it=m.find(5);
+for(auto i=it;i!=m.end();i++){
+    cout<<(*i).first<<endl;
+}
     
 
 }
---
+++ STL/9STLAlgo.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>arr;
+    arr.push_back(56);
+    arr.push_back(12);
+    arr.push_back(564);
+    cout<<"find 12:"<<binary_search(arr.begin(),arr.end(),12)<<endl;
+}
