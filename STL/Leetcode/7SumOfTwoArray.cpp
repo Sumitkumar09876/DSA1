dc589723b06478796f6394fa19f6edac101509fc
Sumitkumar09876 <skumar21-comp@bvucoep.edu.in>
2024-02-06 01:34:54 +0530

    Updated

--- STL/Leetcode/7SumOfTwoArray.cpp
+++ STL/Leetcode/7SumOfTwoArray.cpp
@@ -3,17 +3,62 @@
 using namespace std;
 class sum{
     public:
-    int add(vector<int>arr1,vector<int>arr2){
-        int sum = 0;
-        for(int i=arr1.size()-1,j=arr2.size()-1;i,j<=0;i--,j--){
-            sum += arr1[i] + arr2[j];
-            int temp=sum/10;
-            sum=arr1[i+1]+temp;
+    vector<int>reverse(vector<int>ans){
+        int s=0;
+        int e=ans.size()-1; 
+        while(s<e){ 
+            swap(ans[s++],ans[e--]); 
         }
-        return sum;
+        return ans;
+    }
+    vector<int> add(vector<int>&arr1,vector<int>&arr2){
+        int i=arr1.size()-1;
+        int j=arr2.size()-1;
+        int carry=0;
+        vector<int>ans;
+        while(i>=0 && j>=0){
+            int val1=arr1[i];
+            int val2=arr2[j];
+            int sum=val1+val2+carry;
+            carry=sum/10;
+            sum=sum%10;
+            ans.push_back(sum);
+            i--;
+            j--;
+        }
+         while(i>=0){
+            int sum=arr1[i]+carry;
+            carry=sum/10;
+            sum=sum%10;
+            ans.push_back(sum);
+            i--;
+
+        }
+         while(j>=0){
+            int sum=arr2[j]+carry;
+            carry=sum/10;
+            sum=sum%10;
+            ans.push_back(sum);
+            j--;
+
+        }
+        while(carry !=0){
+            int sum=carry;
+            carry=sum/10;
+            sum=sum%10;
+            ans.push_back(sum);
+
+        }
+        return reverse(ans);
+        
     }
 };
 int main(){
     vector<int>arr1={1,2,3};
     vector<int>arr2={9,9};
+    sum su;
+    vector<int> result = su.add(arr1,arr2);
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i] << " ";
+    }
 }
