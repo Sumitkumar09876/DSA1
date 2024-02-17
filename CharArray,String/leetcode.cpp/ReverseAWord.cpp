7657dce4937f99c8185b19253bb595fa60a56f6d
Sumitkumar09876 <skumar21-comp@bvucoep.edu.in>
2024-02-17 22:12:50 +0530

    update

---
+++ CharArray,String/leetcode.cpp/ReverseAWord.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class reverse{
+    public:
+    string word(string str){
+        int s=0;
+        int e=' ';
+        while(s<)
+    }
+};
+int main(){
+    string str="A man, a plan, a canal: Panama";
+    reverse re;
+    cout<<re.word(str);
+
+}  
