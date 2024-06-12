8dbdc19b19e0828a283b53722dc5363358474a9f
Sumit Kumar <skumar21-comp@bvucoep.edu.in>
2024-06-12 13:14:54 +0530

    updated

--- #Rough/sample.cpp
+++ #Rough/sample.cpp
@@ -1,96 +1,56 @@
 #include<iostream>
 using namespace std;
+
 class Node{
     public:
     int data;
-    Node* next;
+    Node*next;
     Node(int data){
         this->data=data;
         this->next=NULL;
     }
     ~Node(){
         int store=data;
-        if(this->data != NULL){
+        if(data!=NULL){
             delete next;
             this->next=NULL;
         }
+        cout<<"Element deleted:"<<store;
     }
 };
-void InsertAtHead(Node* &head,int data){
-    Node* temp=new Node(data);
-    temp->next=head;
-    head=temp;
-}
-void InsertAtTail(Node* &tail,int data){
-    Node* temp=new Node(data);
-    tail->next=temp;
-    tail=temp;
-}
-void InsertAtMiddle(Node* &head,Node* &tail,int data,int position){
-    if(position==1){
-        return InsertAtHead(head,data);
+void insertNode(Node* &tail,int element,int data){
+    if(tail==NULL){
+        Node* temp=new Node(data);
+        temp->next=temp;
+        tail=temp;
     }
     else{
-        int count=1;
-        Node*temp=head;
-        while(count!=position-1){
-            temp=temp->next;
-            count++;
-        }
-        if(temp==NULL){
-            return InsertAtTail(tail,data);
-        }
-        Node* insertinto=new Node(data);
-        insertinto->next=temp->next;
-        temp->next=insertinto;
-    }
-}
-void DeleteNode(Node* &head,Node* &tail,int position){
-    if(position==1){
-        Node* temp=head;
-        head=head->next;
-        temp->next=NULL;
-        delete temp;
-    }
-    else{
-        int cnt=1;
-        Node* curr=head;
-        Node* prev=NULL;
-        while(cnt<position){
-            prev=curr;
+        Node* curr=tail;
+        while (curr->data!=element)
+        {
             curr=curr->next;
-            cnt++;
         }
-        prev->next=curr->next;
-        curr->next=NULL;
-        delete curr;
-        if(prev->next==NULL){
-            tail=prev;
-        }
+        Node* temp=new Node(data);
+        temp->next=curr->next;
+        curr->next=temp;
     }
 }
-void print(Node* &head){
-    Node* temp=head;
-    while (temp !=NULL)
-    {
+void print(Node* &tail){
+    Node* temp=tail;
+    do{
         cout<<temp->data<<" ";
         temp=temp->next;
-    }
+    }while(temp!=tail);
     cout<<endl;
 }
 int main(){
-Node* newnode=new Node(10);
-Node* head=newnode;
-print(head);
-InsertAtHead(head,9);
-print(head);
-Node* tail=newnode;
-InsertAtTail(tail,11);
-print(head);
-InsertAtMiddle(head,tail,34,2);
-print(head);
-DeleteNode(head,tail,1);
-print(head);
-cout<<head->data<<endl;
-cout<<tail->data;
+    Node* tail=NULL;
+    insertNode(tail,0,10);
+    print(tail);
+    insertNode(tail,10,9);
+    print(tail);
+    insertNode(tail,10,8);
+    print(tail);
+    insertNode(tail,9,7);
+    print(tail);
 }
