f138fc6f8bece3cd9ec1810e53bf5c0377b45bfd
Sumit Kumar <skumar21-comp@bvucoep.edu.in>
2024-11-03 19:11:27 +0530

    updated

--- IBM/Q1.cpp
+++ IBM/Q1.cpp
@@ -3,11 +3,19 @@
 using namespace std;
 class Solution{
     public:
-    int solv(int arr[]){
-        int size=sizeof(arr)/sizeof(arr[0]);
-        for(int i=0;i<size;i++){
-            
-
+    int solv(int arr[],int n){
+        int store=arr[0];
+        for(int i=1;i<n;i++){
+            if(store<arr[i]){
+                store=arr[i];
+            }
         }
+        return store;
     }
+};
+int main(){
+    int arr[]={23,12,45,23,11,5};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    Solution sl;
+    cout<<sl.solv(arr,size);
 }
---
+++ IBM/Q2.cpp
@@ -0,0 +1,9 @@
+//Find Second Smallest and Second Largest Element in an array
+#include<bits/stdc++.h>
+using namespace std;
+class Soltuion{
+    public:
+    int solv(int arr,int n){
+        
+    }
+}
