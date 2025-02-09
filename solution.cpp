0f11e7cc5848a42d598f42b431b9c7d22bbafb35
Sumit Kumar <skumar21-comp@bvucoep.edu.in>
2025-02-09 19:22:04 +0530

    updated

--- solution.cpp
+++ solution.cpp
@@ -1,40 +1,22 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+
 using namespace std;
 
-class Solution {
-public:
-    int solv(vector<int>arr, int size, int k) {
-        int sum = 0, i = 0, j = i + 1;
-        int cnt = 0;
-        
-        while(j < size) {  // Added boundary check
-            sum = arr[i] + arr[j];
-            
-            if(sum < k) {
-                j++;
-            }
-            else if(sum > k) {
-                i++;
-                if(i == j) j++;  // Ensure j stays ahead of i
-            }
-            else if(sum == k) {
-                cout << "Found pair: " << arr[i] << " + " << arr[j] << endl;
-                cnt++;
-                i++;
-                j++;
-            }
-            
-            if(i >= size-1) break;  // Added exit condition
-        }
-        return cnt;
-    }
-};
-
 int main() {
-    vector<int>arr = {2,3,5,1,9};
-    int size = arr.size();
-    Solution sl;
-    cout << "Number of pairs: " << sl.solv(arr,size,10);
+    int n, firstTerm = 0, secondTerm = 1, nextTerm;
+
+    cout << "Enter the number of terms: ";
+    cin >> n;
+
+    cout << "Fibonacci Series: ";
+
+    for (int i = 1; i <= n; i++) {
+        cout << firstTerm << " "; // Print the current term
+        nextTerm = firstTerm + secondTerm; // Calculate the next term
+        firstTerm = secondTerm; // Update firstTerm to be the previous secondTerm
+        secondTerm = nextTerm; // Update secondTerm to be the newly calculated nextTerm
+    }
+    cout << endl;
+
     return 0;
 }
