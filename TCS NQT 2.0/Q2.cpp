146e062da2f7442fa72a6acbeef8baca99813e11
Sumit Kumar <skumar21-comp@bvucoep.edu.in>
2025-02-14 17:52:44 +0530

    Add array-related problems and solutions to questions and code files

---
+++ TCS NQT 2.0/Q2.cpp
@@ -0,0 +1,20 @@
+/*Find second smallest and second largest element in an array
+Problem Statement: Given an array, find the second smallest and second largest element in the array. Print -1 in the event that either of them doesn't exist
+Example 1:
+Input: [1,2,3,7,7,5]
+Output: Second smallest = 2
+Second largest = 5
+*/
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    int solv(int arr[],int size){
+        for(int i=0;i<size;i++){
+            int store=arr[i];
+            for(int j=i;j<size-i;j++){
+                
+            }
+        }
+    }
+}
